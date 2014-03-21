5f91715b9cb76ed58ec75bcfcb0c51e8b609ee9b
Sander Houttekier <sander.houttekier@gmail.com>
2014-03-21 22:28:04 +0100

    Add port 4k into the gruntfile as grunt does not use the config file.

--- gruntfile.js
+++ gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function(grunt) {
                     nodeArgs: ['--debug'],
                     delayTime: 1,
                     env: {
-                        PORT: 3000
+                        PORT: 4000
                     },
                     cwd: __dirname
                 }
