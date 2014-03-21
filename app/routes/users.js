068c64332d6452adc24d64da9f61c32784614260
Sander Houttekier <sander.houttekier@gmail.com>
2014-03-21 23:16:53 +0100

    Register paths for oAuth only when oAuth is not generally disabled

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,6 +2,7 @@
 
 // User routes use users controller
 var users = require('../controllers/users');
+var config = require('../../config/config');
 
 module.exports = function(app, passport) {
 
@@ -22,55 +23,58 @@ module.exports = function(app, passport) {
         failureFlash: true
     }), users.session);
 
-    // Setting the facebook oauth routes
-    app.get('/auth/facebook', passport.authenticate('facebook', {
-        scope: ['email', 'user_about_me'],
-        failureRedirect: '/signin'
-    }), users.signin);
+    if (!config.disableOAuth) {
 
-    app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+        // Setting the facebook oauth routes
+        app.get('/auth/facebook', passport.authenticate('facebook', {
+            scope: ['email', 'user_about_me'],
+            failureRedirect: '/signin'
+        }), users.signin);
 
-    // Setting the github oauth routes
-    app.get('/auth/github', passport.authenticate('github', {
-        failureRedirect: '/signin'
-    }), users.signin);
+        app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+            failureRedirect: '/signin'
+        }), users.authCallback);
 
-    app.get('/auth/github/callback', passport.authenticate('github', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+        // Setting the github oauth routes
+        app.get('/auth/github', passport.authenticate('github', {
+            failureRedirect: '/signin'
+        }), users.signin);
 
-    // Setting the twitter oauth routes
-    app.get('/auth/twitter', passport.authenticate('twitter', {
-        failureRedirect: '/signin'
-    }), users.signin);
+        app.get('/auth/github/callback', passport.authenticate('github', {
+            failureRedirect: '/signin'
+        }), users.authCallback);
 
-    app.get('/auth/twitter/callback', passport.authenticate('twitter', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+        // Setting the twitter oauth routes
+        app.get('/auth/twitter', passport.authenticate('twitter', {
+            failureRedirect: '/signin'
+        }), users.signin);
 
-    // Setting the google oauth routes
-    app.get('/auth/google', passport.authenticate('google', {
-        failureRedirect: '/signin',
-        scope: [
-            'https://www.googleapis.com/auth/userinfo.profile',
-            'https://www.googleapis.com/auth/userinfo.email'
-        ]
-    }), users.signin);
+        app.get('/auth/twitter/callback', passport.authenticate('twitter', {
+            failureRedirect: '/signin'
+        }), users.authCallback);
 
-    app.get('/auth/google/callback', passport.authenticate('google', {
-        failureRedirect: '/signin'
-    }), users.authCallback);
+        // Setting the google oauth routes
+        app.get('/auth/google', passport.authenticate('google', {
+            failureRedirect: '/signin',
+            scope: [
+                'https://www.googleapis.com/auth/userinfo.profile',
+                'https://www.googleapis.com/auth/userinfo.email'
+            ]
+        }), users.signin);
 
-    // Setting the linkedin oauth routes
-    app.get('/auth/linkedin', passport.authenticate('linkedin', {
-        failureRedirect: '/signin',
-        scope: [ 'r_emailaddress' ]
-    }), users.signin);
+        app.get('/auth/google/callback', passport.authenticate('google', {
+            failureRedirect: '/signin'
+        }), users.authCallback);
 
-    app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
-        failureRedirect: '/siginin'
-    }), users.authCallback);
+        // Setting the linkedin oauth routes
+        app.get('/auth/linkedin', passport.authenticate('linkedin', {
+            failureRedirect: '/signin',
+            scope: [ 'r_emailaddress' ]
+        }), users.signin);
+
+        app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
+            failureRedirect: '/siginin'
+        }), users.authCallback);
+    }
 
 };
